91546e3db01299d2af70ccfc7a4112d4975359ad
John Conroy <johnconroy29@gmail.com>
2020-01-28 13:33:40 -0500

    Create test lambda function

---
+++ lambdas/hello-world/lambda_function.py
@@ -0,0 +1,15 @@
+import boto3
+
+def handler(event, context):
+    body = "Hello from API Gateway!"
+    ret = {
+        "statusCode" : "200" ,
+        "headers" : {
+            "Content-Type" : "application/json" ,
+            "Access-Control-Allow-Headers" : 'Content-Type' ,
+            "Access-Control-Allow-Origin" : "*" ,
+            "Access-Control-Allow-Methods" : "POST, OPTIONS" ,
+            "Access-Control-Allow-Credentials" : True
+        },
+        "body": body}
+    return ret
