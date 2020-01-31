8163b1fb83c37902ada48a0257a72cc71f493926
John Conroy <johnconroy29@gmail.com>
2020-01-30 22:22:14 -0500

    Add routes for new containers

--- expo-front-end/src/Routes.js
+++ expo-front-end/src/Routes.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home.js";
+import AR from "./containers/AR.js";
+import Map from "./containers/Map.js";
+import Projects from "./containers/Projects.js";
+import Upload from "./containers/Upload.js";
 
 const Routes = ({ childProps }) => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/AR" exact component={AR} />
+      <Route path="/Map" exact component={Map} />
+      <Route path="/Projects" exact component={Projects} />
+      <Route path="/Upload" exact component={Upload} />
     </Switch>
   );
 };
