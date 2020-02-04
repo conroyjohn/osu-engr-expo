8ebe6e4fc98efcd23322eb8192eabfa57df30b71
John Conroy <johnconroy29@gmail.com>
2020-02-04 14:07:55 -0500

    Import aws-amplify and config file to index

--- expo-front-end/src/index.js
+++ expo-front-end/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import Amplify from "aws-amplify";
 import "./index.css";
 import App from "./App";
+import config from "./config";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
