d3aba11dcb958f84b5d0892d80182ff6a08b0dac
John Conroy <johnconroy29@gmail.com>
2020-02-09 11:55:32 -0500

    Remove leading space from amplify config file

--- expo-front-end/src/config.js
+++ expo-front-end/src/config.js
@@ -5,7 +5,7 @@ export default {
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: " us-east-1_bvRqWoLuY",
+    USER_POOL_ID: "us-east-1_bvRqWoLuY",
     APP_CLIENT_ID: "m8ckmps8b2mtq2kgblplj7m19",
     IDENTITY_POOL_ID: "us-east-1:a06ebbcf-d46b-4c6e-ae1b-3d557cac3802"
   }
