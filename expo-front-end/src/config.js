64bbac52ac9a9f788d5b9f8ba5bebc4d7c2c117a
John Conroy <johnconroy29@gmail.com>
2020-02-09 09:49:04 -0500

    Update amplify config file

--- expo-front-end/src/config.js
+++ expo-front-end/src/config.js
@@ -1,16 +1,12 @@
 export default {
-  s3: {
-    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
-  },
   apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
+    REGION: "us-east-1",
+    URL: "https://v5yyja3u9i.execute-api.us-east-1.amazonaws.com/v0"
   },
   cognito: {
-    REGION: "YOUR_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    REGION: "us-east-1",
+    USER_POOL_ID: " us-east-1_bvRqWoLuY",
+    APP_CLIENT_ID: "m8ckmps8b2mtq2kgblplj7m19",
+    IDENTITY_POOL_ID: "us-east-1:a06ebbcf-d46b-4c6e-ae1b-3d557cac3802"
   }
 };
