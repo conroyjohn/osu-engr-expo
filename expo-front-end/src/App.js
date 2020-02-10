f65d153a22b8c68703e641deeb0dd0931dbf4f67
John Conroy <johnconroy29@gmail.com>
2020-02-10 08:41:48 -0500

    Add authentication to App state and pass needed props to login and logout children

--- expo-front-end/src/App.js
+++ expo-front-end/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
 import { StylesProvider, ThemeProvider } from "@material-ui/styles";
 import { CssBaseline, createMuiTheme } from "@material-ui/core";
 import styled, {
   ThemeProvider as StyledThemeProvider
 } from "styled-components";
-
+import { withRouter } from "react-router-dom";
 import Routes from "./Routes.js";
 import Nav from "./components/Nav.js";
 
@@ -15,23 +16,54 @@ const theme = createMuiTheme({
   palette: { primary: { main: "#d73f09" } }
 });
 
-const App = () => {
+const App = props => {
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  const onLoad = async () => {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  };
+
+  const handleLogout = async () => {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    props.history.push("/login");
+  };
+
   return (
     // Tell material-ui to inject its styles first so that
     // styled-components can override/extend the styles, else
     // material-ui will override
-    <StylesProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        {/* pass theme to styled-components so it can be used
+    !isAuthenticating && (
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          {/* pass theme to styled-components so it can be used
       (see StyledPaper above) */}
-        <StyledThemeProvider theme={theme}>
-          <CssBaseline />
-          <Nav />
-          <Routes />
-        </StyledThemeProvider>
-      </ThemeProvider>
-    </StylesProvider>
+          <StyledThemeProvider theme={theme}>
+            <CssBaseline />
+            <Nav
+              handleLogout={handleLogout}
+              isAuthenticated={isAuthenticated}
+            />
+            <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+          </StyledThemeProvider>
+        </ThemeProvider>
+      </StylesProvider>
+    )
   );
 };
 
-export default App;
+export default withRouter(App);
