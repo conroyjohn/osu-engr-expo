b8eeac08cf175619de4a0dc588f7cebd6eeede29
John Conroy <johnconroy29@gmail.com>
2020-02-10 16:54:33 -0500

    Reformat Projects container

--- expo-front-end/src/containers/Projects.js
+++ expo-front-end/src/containers/Projects.js
@@ -1,53 +1,54 @@
-import React, { Component} from "react";
-import '../App.css';
-import TableFilter from 'react-table-filter';
-
+import React, { Component } from "react";
+import "../App.css";
+import TableFilter from "react-table-filter";
 
 class Table extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       projects: [
-        { Team: 'Alpha', Project: 'Saving the Planet', Department: 'Environmental Science', Booth: 41}
+        {
+          Team: "Alpha",
+          Project: "Saving the Planet",
+          Department: "Environmental Science",
+          Booth: 41
+        }
       ]
-   }
+    };
   }
-  renderTableHeader(){
-    let header = Object.keys(this.state.projects[0])
+  renderTableHeader() {
+    let header = Object.keys(this.state.projects[0]);
     return header.map((key, index) => {
-      return <th key = {index}>{key.toUpperCase()}</th>
-    })
+      return <th key={index}>{key.toUpperCase()}</th>;
+    });
   }
 
   renderTableData() {
-      return this.state.projects.map((projects, index)=> {
-        const { Team, Project, Department, Booth } = projects
-        return (
-	  <tr key={Team}>
+    return this.state.projects.map((projects, index) => {
+      const { Team, Project, Department, Booth } = projects;
+      return (
+        <tr key={Team}>
           <td>{Team}</td>
           <td>{Project}</td>
-	  <td>{Department}</td>
+          <td>{Department}</td>
           <td>{Booth}</td>
-          </tr>
-	)
-    })
+        </tr>
+      );
+    });
   }
 
-
-  render () {
+  render() {
     return (
-       <div>
-          <h1 id = 'title'>Projects</h1>
-          <table id ='projects'>
- 	    <tbody>
-                <tr>{this.renderTableHeader()}</tr>
-		{this.renderTableData()}
-	    </tbody>
-	  </table>
-	</div>
-    )
-	}
-      
+      <div>
+        <h1 id="title">Projects</h1>
+        <table id="projects">
+          <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-export default Table
-
+export default Table;
