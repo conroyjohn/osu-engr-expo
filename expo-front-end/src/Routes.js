fdc4826efb75a93cafb6ffca8cb9083cd6511036
John Conroy <johnconroy29@gmail.com>
2020-02-10 08:45:37 -0500

    Add login, change password and hello world containers to routes

--- expo-front-end/src/Routes.js
+++ expo-front-end/src/Routes.js
@@ -1,19 +1,47 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute.js";
+import AuthenticatedRoute from "./components/AuthenticatedRoute.js";
 import Home from "./containers/Home.js";
 import AR from "./containers/AR.js";
 import Map from "./containers/Map.js";
 import Projects from "./containers/Projects.js";
 import Upload from "./containers/Upload.js";
+import Login from "./containers/Login.js";
+import HelloWorld from "./containers/HelloWorld.js";
+import ChangePassword from "./containers/ChangePassword.js";
 
-const Routes = ({ childProps }) => {
+const Routes = ({ appProps }) => {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/AR" exact component={AR} />
-      <Route path="/Map" exact component={Map} />
-      <Route path="/Projects" exact component={Projects} />
-      <Route path="/Upload" exact component={Upload} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/LogIn" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/AR" exact component={AR} appProps={appProps} />
+      <AppliedRoute
+        path="/Projects"
+        exact
+        component={Projects}
+        appProps={appProps}
+      />
+      <AppliedRoute path="/Map" exact component={Map} appProps={appProps} />
+      <AppliedRoute
+        path="/Upload"
+        exact
+        component={Upload}
+        appProps={appProps}
+      />
+      <AppliedRoute
+        path="/changePassword"
+        exact
+        component={ChangePassword}
+        appProps={appProps}
+      />
+      <AppliedRoute
+        path="/HelloWorld"
+        exact
+        component={HelloWorld}
+        appProps={appProps}
+      />
     </Switch>
   );
 };
