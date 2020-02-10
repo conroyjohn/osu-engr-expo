900002f26dff5a1b93c18b1ef64528e39539f963
John Conroy <johnconroy29@gmail.com>
2020-02-10 08:50:53 -0500

    Comment out XR config untill working in index.js

--- expo-front-end/src/index.js
+++ expo-front-end/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Amplify from "aws-amplify";
-import awsconfig from './aws-exports';
-import scene1Config from './sumerian_exports_cb090715cfee47f7ac00fbc14d5a9e66.scene'; 
+//import awsconfig from "./aws-exports";
+//import scene1Config from "./sumerian_exports_cb090715cfee47f7ac00fbc14d5a9e66.scene";
 import "./index.css";
 import App from "./App";
 import config from "./config";
@@ -25,14 +25,15 @@ Amplify.configure({
         region: config.apiGateway.REGION
       }
     ]
-  }
-    ...awsconfig,
-  XR: { // XR category configuration
-    region: 'us-east-2', // Sumerian region
-    scenes: { 
-      "scene1": { // Friendly scene name
+  },
+  XR: {
+    // XR category configuration
+    region: "us-east-2", // Sumerian region
+    scenes: {
+      /*scene1: {
+        // Friendly scene name
         sceneConfig: scene1Config // Scene configuration from Sumerian publish
-      }
+      } */
     }
   }
 });
@@ -43,7 +44,6 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
