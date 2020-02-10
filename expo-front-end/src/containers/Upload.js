b26cac3d9ec427e0e89163e220a16e5fd37417a5
ryagomes <ryagomes>
2020-02-10 18:24:00 -0500

    added project input form

--- expo-front-end/src/containers/Upload.js
+++ expo-front-end/src/containers/Upload.js
@@ -5,34 +5,99 @@ import '../App.css';
 class MyForm extends React.Component {
   constructor() {
     super();
+    //initialize the state
     this.state = {
         name:'',
         description:'',
         picture:'',
-        team:'',
+        team:[{ email : "" }],
         school:'',
         tech:'',
         college:'',
-        links:''
+        links:[{ name : "",link:"" }]
     }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  //Following 4 funtions for handling team input
+  handleAddTeamMember = () => {
+      this.setState({
+          team: this.state.team.concat([{ email: "" }])
+      });
+  }
+
+
+  handleRemoveTeamMember = idx => () =>{
+      this.setState({
+          team: this.state.team.filter((s, sidx) => idx !== sidx)
+      });
+  }
+
+  handleTeamMemberChange = idx => evt => {
+    const newTeamMembers = this.state.team.map((teamMember, sidx) => {
+      if (idx !== sidx) return teamMember;
+      return { ...teamMember, email: evt.target.value };
+    });
+
+    this.setState({ team: newTeamMembers });
+  };
+
+
+
+  //Following 4 funtions for handling input reference links
+  handleAddLink = () => {
+      this.setState({
+          links: this.state.links.concat([{ name : "",link:"" }])
+      });
+  }
+
+
+  handleRemoveLink = idx => () =>{
+      this.setState({
+          links: this.state.links.filter((s, sidx) => idx !== sidx)
+      });
+  }
+
+  handleLinkDescChange = idx => evt => {
+      console.log(evt.target)
+    const newLinks = this.state.links.map((link, sidx) => {
+      if (idx !== sidx) return link;
+      return { ...link, name: evt.target.value };
+    });
+
+    this.setState({ links: newLinks });
+  };
+
+
+  handleLinkWebsiteChange = idx => evt => {
+      console.log(evt.target)
+    const newLinks = this.state.links.map((link, sidx) => {
+      if (idx !== sidx) return link;
+      return { ...link, link: evt.target.value };
+    });
+
+    this.setState({ links: newLinks });
+  };
+
   handleSubmit(event) {
     event.preventDefault();
 
+    let team = this.state.team
+    let inputLinks = this.state.links
 
     this.setState({
         name:event.target.projectName.value,
         description:event.target.projectDescription.value,
         picture:event.target.projectPicture.files[0],
-        team:'',
+        team:team,
         school:event.target.projectSchool.value,
         tech:event.target.projectTech.value,
         college:event.target.projectCollege.value,
-        links:''
+        links:inputLinks
     })
+    console.log("Data was submitted!!")
     console.log(this.state)
+    
     // fetch('/api/form-submit-url', {
     //   method: 'POST',
     //   body: data,
@@ -69,14 +134,80 @@ class MyForm extends React.Component {
     	<br></br>
 
 
-        <label htmlFor="projectTeam">Project Team</label>
-        <input id="projectTeam" name="projectTeam" type="text" />
-    	<br></br>
+        <label>Team Members</label>
+        <br></br>
+
+        {this.state.team.map((teamMember, idx) => (
+          <div key={idx+1}>
+            <input
+              type="email"
+              placeholder={`Team member #${idx + 1} name`}
+              value={teamMember.email}
+              onChange={this.handleTeamMemberChange(idx)}
+            />
+
+            <button
+              type="button"
+              onClick={this.handleRemoveTeamMember(idx)}
+              className="small">
+              -
+            </button>
+
+          </div>
+        ))}
+
+        <button
+          type="button"
+          onClick={this.handleAddTeamMember}
+          className="small"
+        >
+          Add Team Member
+        </button>
+
+        <br></br>
 
 
-        <label htmlFor="projectLinks">Project Links</label>
-        <input id="projectLinks" name="projectLinks" type="text" />
-    	<br></br>
+
+
+
+        <label>Reference links for the projet</label>
+        <br></br>
+
+        {this.state.links.map((link, idx) => (
+          <div key={idx+1}>
+            <input
+              type="text"
+              placeholder={`Link #${idx + 1} description`}
+              value={link.name}
+              onChange={this.handleLinkDescChange(idx)}
+            />
+            <input
+              type="text"
+              placeholder={`Link #${idx + 1} website`}
+              value={link.link}
+              onChange={this.handleLinkWebsiteChange(idx)}
+            />
+
+            <button
+              type="button"
+              onClick={this.handleRemoveLink(idx)}
+              className="small">
+              -
+            </button>
+
+          </div>
+        ))}
+
+        <button
+          type="button"
+          onClick={this.handleAddLink}
+          className="small"
+        >
+          Add Link
+        </button>
+
+        <br></br>
+
 
     	<button>Send data</button>
     </form>
