bdb2f900fbdab53c3bddbbdc55439eb87f406793
Danyelle <DanyelleMontalvo@gmail.com>
2020-02-09 19:51:36 -0700

    Front end Table and Form

--- expo-front-end/src/containers/Projects.js
+++ expo-front-end/src/containers/Projects.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { Component} from "react";
+import '../App.css';
+import TableFilter from 'react-table-filter';
 
-const Projects = () => {
-  return <div>Projects page</div>;
-};
 
-export default Projects;
+class Table extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      projects: [
+        { Team: 'Alpha', Project: 'Saving the Planet', Department: 'Environmental Science', Booth: 41}
+      ]
+   }
+  }
+  renderTableHeader(){
+    let header = Object.keys(this.state.projects[0])
+    return header.map((key, index) => {
+      return <th key = {index}>{key.toUpperCase()}</th>
+    })
+  }
+
+  renderTableData() {
+      return this.state.projects.map((projects, index)=> {
+        const { Team, Project, Department, Booth } = projects
+        return (
+	  <tr key={Team}>
+          <td>{Team}</td>
+          <td>{Project}</td>
+	  <td>{Department}</td>
+          <td>{Booth}</td>
+          </tr>
+	)
+    })
+  }
+
+
+  render () {
+    return (
+       <div>
+          <h1 id = 'title'>Projects</h1>
+          <table id ='projects'>
+ 	    <tbody>
+                <tr>{this.renderTableHeader()}</tr>
+		{this.renderTableData()}
+	    </tbody>
+	  </table>
+	</div>
+    )
+	}
+      
+}
+export default Table
+
--- expo-front-end/src/containers/Upload.js
+++ expo-front-end/src/containers/Upload.js
@@ -1,7 +1,47 @@
 import React from "react";
+import REACTDOM from "react-dom";
+import '../App.css';
 
-const Upload = () => {
-  return <div>Upload page</div>;
-};
+class MyForm extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default Upload;
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(event.target.files[0])
+    fetch('/api/form-submit-url', {
+      method: 'POST',
+      body: data,
+    });
+  }
+
+  render() {
+    return (
+      <form id="subform" onSubmit={this.handleSubmit}>
+        <label htmlFor="username">Enter username</label>
+        <input id="username" name="username" type="text" />
+	<br></br>
+	<label htmlFor="onid">Enter ONID</label>
+	<input id="oonid" name="onid" type ="text" />
+	<br></br>
+        <label htmlFor="email">Enter your email</label>
+        <input id="email" name="email" type="email" />
+	<br></br>
+        <label htmlFor="projectname">Enter Project name</label>
+        <input id="projectname" name="projectname" type="text" />
+	<br></br>
+	<label htmlFor="description"> Description: </label>
+	<textarea>Enter in a description of the project here</textarea>
+	<br></br>
+	<label>Upload Your File</label>
+	<input type="file" name="file" onChange={this.handleSubmit}/>
+        <br></br>
+	 <button>Send data</button>
+      </form>
+    );
+  }
+}
+export default MyForm;
