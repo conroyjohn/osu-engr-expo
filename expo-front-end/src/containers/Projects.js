cba55c11d6df77c33d58b24884d9f09cfb7f6b2d
Daniel Lindsay <daniel.lindsay18@gmail.com>
2020-02-22 15:05:34 -0500

    Update Projects.js

--- expo-front-end/src/containers/Projects.js
+++ expo-front-end/src/containers/Projects.js
@@ -6,26 +6,10 @@ export default class Table extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      /*projects: [
-        {
-          Team: "Alpha",
-          Project: "Saving the Planet",
-          Department: "Environmental Science",
-          Booth: 41
-        }
-      ]*/
+      isLoading: true,
+      data: {}
     };
   }
-  
-  /*
-  renderTableHeader() {
-    let header = Object.keys(this.state.projects[0]);
-    return header.map((key, index) => {
-      return <th key={index}>{key.toUpperCase()}</th>;
-    });
-  }
-  */
   
   renderTableHeader(columnHeads) {
     return columnHeads.map((key, index) => {
@@ -33,45 +17,48 @@ export default class Table extends Component {
     });
   }
 
-  renderTableData(projectList) {
-    return projectList.map((projects, key) => {
+  renderTableData() {
+    return this.state.data.map((projects, key) => {
       return (
         <tr key={key}>
           <td>{projects.name}</td>
           <td>{projects.school}</td>
           <td>{projects.college}</td>
-          <td>{projects.team}</td>
+          <td>{projects.description}</td>
         </tr>
       );
     });
-    
   }
   
-  	async getData() {
+	async componentDidMount() {
 		try {
-       const data = await fetch('https://v5yyja3u9i.execute-api.us-east-1.amazonaws.com/v0/get-all-projects');
-       let projectList = data.json();
+			const response = await fetch('https://v5yyja3u9i.execute-api.us-east-1.amazonaws.com/v0/get-all-projects');
+			let responseJson = await response.json();
 			this.setState(
 				{
-					data: projectList
+					isLoading: false,
+					data: responseJson
 				},
 				function() {}
 			);
 		} catch (error) {
-			console.error('Error');
+			console.error(error);
 		}
 	}
 
   render() {
-    
 
     const columnHeads = [
     'name',
     'school',
     'college',
-    'team',
+    'description',
   ];
-    
+
+  if (this.state.isLoading) {
+    return <div>Loading...</div>;
+  } else { 
+
     return (
       <div>
         <h1 id="title">Projects</h1>
@@ -80,12 +67,13 @@ export default class Table extends Component {
           <tr>{this.renderTableHeader(columnHeads)}</tr>
           </thead>
           <tbody>
-            {this.renderTableData(this.state.data)}
+            {this.renderTableData()}
           </tbody>
         </table>
       </div>
     );
   }
+}
 }
 
 // Daniel Lindsay - Referenced this for the API code: https://medium.com/serverlessguru/serverless-api-with-reactjs-6fa297ac8a27
