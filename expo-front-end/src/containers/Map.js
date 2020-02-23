214e5832586cffbbd67630a4a0609a395181a002
Danyelle <DanyelleMontalvo@gmail.com>
2020-02-23 15:52:07 -0700

    Daanyelle's Map

--- expo-front-end/src/containers/Map.js
+++ expo-front-end/src/containers/Map.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React from 'react';
+import expo from '../Images/trade-show-floor-plan.png'; 
+import Image from'react-image-resizer';
+import '../App.css';
+import Grid from '@material-ui/core/Grid';
+import ReactTooltip from 'react-tooltip';
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Tooltip from '@material-ui/core/Tooltip';
+import {Row,Col} from 'react-flexbox-grid';
 
-const Map = () => {
-  return <div>Map page</div>;
+var sectionStyle = {
+	width: "100%",
+	height: "400px",
+	backgroundImage: 'url(${expo})'
 };
+
+class Map extends React.Component{
+
+	constructor(props) {
+	  super(props);
+	  this.state ={
+	    open: false
+	  };
+        }
+
+render(){
+const handleTooltipClose = () => {
+    this.setState({
+	open: false
+    });
+};
+
+  const handleTooltipOpen = () => {
+    this.setState({
+	open: true
+    });
+};
+
+return(
+	<section style={sectionStyle}>
+		
+<Grid item>
+ <Row>
+ <Col xs={6} md={3}>
+ 
+ </Col>
+ <Col xs={6} md={3}>
+
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <div>
+              <Tooltip
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                open={this.state.open}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                title="Team Name: Rockstars/n Project Description: Blah blahblah"
+              >
+                <Button onClick={handleTooltipOpen}> 
+		<img src={expo}/>
+		</Button>
+              </Tooltip>
+            </div>
+          </ClickAwayListener>
+ </Col>
+</Row>
+</Grid>
+	</section>);	
+}
+}
 
 export default Map;
