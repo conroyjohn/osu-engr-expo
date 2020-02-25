9408546e6fc0d1f60314f8d6092768c67030d99a
Danyelle <DanyelleMontalvo@gmail.com>
2020-02-24 21:58:14 -0700

    Map features with Multiline Tooltip

--- expo-front-end/src/containers/Map.js
+++ expo-front-end/src/containers/Map.js
@@ -9,11 +9,12 @@ import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Tooltip from '@material-ui/core/Tooltip';
 import {Row,Col} from 'react-flexbox-grid';
 
+/*
 var sectionStyle = {
 	width: "100%",
-	height: "400px",
-	backgroundImage: 'url(${expo})'
+	height: "400px"
 };
+*/
 
 class Map extends React.Component{
 
@@ -38,39 +39,66 @@ const handleTooltipClose = () => {
 };
 
 return(
-	<section style={sectionStyle}>
-		
-<Grid item>
- <Row>
- <Col xs={6} md={3}>
- 
- </Col>
- <Col xs={6} md={3}>
+ <div style={{ //backgroundImage:`url(require(${expo}))`,
+		    backgroundImage: "url(/trade-show-floor-plan.png)",
+	            height:"500px",
+		    width:"100%",
+		    backgroundposition: "center",
+		    position: "relative",
+		    backgroundRepeat: "no-repeat" }} >
+<div style={{ position:"relative", top:"18%", left:"9%"}}> 
+<button ref={ref => this.sooRef = ref} 
+//When integrating use this section of code:
+//use<ReactTooltip id='test' getContent={() => { return null }}/>
+data-tip='Team Name: Art Attacks<br /> Project Name: Saving the word from Cyber Attack <br /> Project description: This project is about securing the world of tomorrow from nationwide attacks using blah blah blah' data-place= 'right' data-type= 'info' text-align= 'left' data-multiline ='true' onClick={() => {ReactTooltip.show(this.sooRef) }} data-event='click'> </button>
+<ReactTooltip />
 
-          <ClickAwayListener onClickAway={handleTooltipClose}>
-            <div>
-              <Tooltip
-                PopperProps={{
-                  disablePortal: true,
-                }}
-                onClose={handleTooltipClose}
-                open={this.state.open}
-                disableFocusListener
-                disableHoverListener
-                disableTouchListener
-                title="Team Name: Rockstars/n Project Description: Blah blahblah"
-              >
-                <Button onClick={handleTooltipOpen}> 
-		<img src={expo}/>
-		</Button>
-              </Tooltip>
+<div style={{ position:"relative", top:"1%", left:"12.5%", width: '50px', height: '30px'}}> 
+<button style={{ background: 'transparent', outline: 'none', border: 'none', height: '100%', width: '100%'}} ref={ref => this.mooRef = ref} 
+//When integrating use this section of code:
+//use<ReactTooltip id='test' getContent={() => { return null }}/>
+data-tip='put Data in here' data-place= 'right' data-type= 'info' text-align= 'left' data-multiline ='true' onClick={() => {ReactTooltip.show(this.mooRef) }} data-event='click'> </button>
+<ReactTooltip />
+</div>
+
+</div>
+<div style={{ position:"relative", top:"8%", left:"12.25%", width: '50px', height: '30px'}}> 
+<button style={{ 
+  //background: 'transparent', 
+  outline: 'none', border: 'none', height: '100%', width: '100%'}} ref={ref => this.fooRef = ref} 
+//When integrating use this section of code:
+//use<ReactTooltip id='test' getContent={() => { return null }}/> Delete the string data in data-tip
+data-tip='Team Name: Art Attacks<br /> Project Name: Saving the word from Cyber Attack <br /> Project description: This project is about securing the world of tomorrow from nationwide attacks using blah blah blah' data-place= 'right' data-type= 'info' text-align= 'left' data-multiline ='true' onClick={() => {ReactTooltip.show(this.fooRef) }} data-event='click'> </button>
+<ReactTooltip />
+</div>
+
+    <div style={{ position:"relative", top:"26%", left:"9%"}}> 
+             <button ref={ref => this.looRef = ref} data-tip='Team Name: Tim sucks<br /> Project Name: Tim sucks so much i do not know how he can still have a mouth because he sucks so much' data-place= 'right' data-type= 'info' text-align= 'left' data-multiline ='true' onClick={() => {ReactTooltip.show(this.looRef) }} data-event='click'> </button>
+<ReactTooltip />
             </div>
-          </ClickAwayListener>
- </Col>
-</Row>
-</Grid>
-	</section>);	
+     
+ 	</div>);	
 }
+};
+
+
+
+
+function App() {
+   return (
+      <div style={{ //backgroundImage:`url(require(${expo}))`,
+		    backgroundImage: "url(/trade-show-floor-plan.png)",
+	            height:"401px",
+		    width:"100%",
+		    backgroundposition: "center",
+		    backgroundRepeat: "no-repeat" }} >
+         <h1> Hello <br/>
+	Hello2 <br/>
+	Hello3 <br/>
+	Hello4</h1>
+	
+      </div>
+   );
 }
 
 export default Map;
